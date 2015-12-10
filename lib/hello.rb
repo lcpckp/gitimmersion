482676f58c055d45f4e463d673382ae72131b4a2
Alec Pickup <lcpckp@gmail.com>
2015-12-09 20:40:19 -0500

    Hello uses Greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,10 @@
-# Default is "World"
+require 'greeter'
+
+# Default is World
+name = ARGV.first || "World"
+
+greeter = Greeter.new(name)
+puts greeter.greet# Default is "World"
 # Author: Alec Pickup (apickup@uncc.edu)
 name = ARGV.first || "World"
 
